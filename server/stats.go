a78a15ee4a1690983fa382146364e2f1f84fac95
Sebastian Pinery <sebapinery@gmail.com>
2025-08-24 13:44:56 -0300

    fix: rollback changes in stats

--- server/stats.go
+++ server/stats.go
@@ -1,16 +1,13 @@
 package server
 
 import (
-	"context"
 	"encoding/json"
 	"log"
 	"net"
 	"net/http"
 	"sync/atomic"
-	"time"
 )
 
-// Este archivo está reservado para futuras implementaciones de estadísticas // Agregar tipos de stats.go
 type statistics struct {
 	Connections connections `json:"connections"`
 	Topics      topics      `json:"topics"`
@@ -29,10 +26,6 @@ type connections struct {
 }
 
 func (s *Server) handleStats(w http.ResponseWriter, _ *http.Request) {
-	// Usar context y time para eliminar advertencias
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
 	stats := statistics{
 		Connections: connections{},
 		Topics:      make(map[string]topicDetail),
@@ -41,27 +34,21 @@ func (s *Server) handleStats(w http.ResponseWriter, _ *http.Request) {
 	conns := make(map[net.Conn]bool)
 
 	for topic, cc := range s.clients {
-		select {
-		case <-ctx.Done():
-			log.Println("Context cancelled while processing stats")
-			return
-		default:
-			for _, c := range cc {
-				_, ok := conns[c]
-				if !ok {
-					conns[c] = true
-					stats.Connections.TotalConnected++
-				}
-			}
-
-			_, ok := stats.Topics[topic.Name]
+		for _, c := range cc {
+			_, ok := conns[c]
 			if !ok {
-				sentMsgs := s.sentMessages[topic]
+				conns[c] = true
+				stats.Connections.TotalConnected++
+			}
+		}
 
-				stats.Topics[topic.Name] = topicDetail{
-					Subscribers:  len(cc),
-					MessagesSent: sentMsgs.Load(),
-				}
+		_, ok := stats.Topics[topic.Name]
+		if !ok {
+			sentMsgs := s.sentMessages[topic]
+
+			stats.Topics[topic.Name] = topicDetail{
+				Subscribers:  len(cc),
+				MessagesSent: sentMsgs.Load(),
 			}
 		}
 	}
